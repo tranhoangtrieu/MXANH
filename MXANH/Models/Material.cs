b0e06abaf2ee72eabd2d50404655eb9255c45f47
tranhoangtrieu <tranhoangtrieu03092003@gmail.com>
2025-06-03 09:56:26 +0700

    update Material images

--- MXANH/Models/Material.cs
+++ MXANH/Models/Material.cs
@@ -6,9 +6,9 @@
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal PricePerKg { get; set; }
-        public string ImageUrl { get; set; }
         public DateTime CreatedAt { get; set; }
 
+        public ICollection<MaterialImage> Images { get; set; }
         public ICollection<CollectionRequest> CollectionRequests { get; set; }
     }
 
